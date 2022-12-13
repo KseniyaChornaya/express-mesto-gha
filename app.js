cfacbd23f28c3d1900664515c87c77d852bb30b4
Kseniya Chornaya <ksu.chornaya@gmail.com>
2022-12-13 18:14:49 +0300

    add middlewar user id

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ const { PORT = 3000, MONGO_URL = 'mongodb://localhost:27017/mestodb' } =
   process.env;
 const app = express();
 app.use(express.json());
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6396fbecf369fb5dc2b758ec',
+  };
 
+  next();
+});
 app.use(routes);
-
 async function connect() {
   await mongoose.connect(MONGO_URL);
   console.log(`App connect db ${MONGO_URL}`);
